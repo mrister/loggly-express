16ca9d620fc40b1093a5e80ff5b24c2b33901219
Mihovil Rister <mihovil.rister@gmail.com>
2016-08-10 22:52:45 +0200

    Update package.json with engine, license, version and private information.

--- routes/webhooks/500.js
+++ routes/webhooks/500.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.post('*', function(req, res, next) {
+router.post('*', function(req, res) {
   var body = typeof req.body === 'string' ? JSON.parse(req.body) : req.body;
   // do something useful like log, close the offending route, start throttling affected route etc.
    console.log('[ALERT]: We got ' + body['num_hits'] + ' hits. Please contact ' + body['owner_email']+ ' and let him take a look at ' + body['search_link']);
